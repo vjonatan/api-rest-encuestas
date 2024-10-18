13255bde755628f5a341776992d02f8a41fb3df7
Jonatan <jonatan.e.vera@gmail.com>
2024-10-18 06:14:35 -0300

    config inicial

---
+++ src/test/java/com/api/rest/ApiRestEncuestasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestEncuestasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
